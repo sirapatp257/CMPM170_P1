a96ac229e5453da6984d9e3e344233be95a9d31c
wichapas <wpichetp@ucsc.edu>
2023-10-16 16:40:19 -0700

    Harry: Updated Collision Detection for border 10/16/23

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ class Balloon {
             .setScale(3)
             .setGravityY(0) // Change gravity value here
             .setInteractive()
+            .setCollideWorldBounds(true, 1, 1, true)
             .on("pointerdown", ()=>{
                 console.log("clicked on balloon!");
                 scene.counter.setText(`Click count: ${++scene.clickCount}`);
@@ -22,6 +23,28 @@ class PrototypeScene extends Phaser.Scene {
 
     create() {
         this.balloon = new Balloon(this, 320, 180);
+
+        this.balloon.sprite.onWorldBounds = true;
+        
+        this.physics.world.on('worldbounds', (body, up, down, left, right) => {
+            //insert what happens when the balloon touches the edges here
+            if(up){
+                console.log("up test");
+            }
+            if(down){
+                console.log("down test");
+            }
+            if(left){
+                console.log("left test");
+            }
+            if(right){
+                console.log("right test");
+            }
+        });
+
+        //for testing
+        this.cursors = this.input.keyboard.createCursorKeys();
+
         
         this.clickCount = 0;
         this.counter = this.add.text(30, 20, `Click count: ${this.clickCount}`);
@@ -29,6 +52,31 @@ class PrototypeScene extends Phaser.Scene {
 
     update() {
         // Add to this function as needed
+        //Harry:i added arrow key movement for the sake of testng collission
+        {
+            this.balloon.sprite.setVelocity(0);
+
+            if (this.cursors.left.isDown)
+            {
+                this.balloon.sprite.setVelocityX(-300);
+            }
+            else if (this.cursors.right.isDown)
+            {
+                this.balloon.sprite.setVelocityX(300);
+                
+            }
+
+            if (this.cursors.up.isDown)
+            {
+                this.balloon.sprite.setVelocityY(-300);
+            
+            }
+            else if (this.cursors.down.isDown)
+            {
+                this.balloon.sprite.setVelocityY(300);
+                
+            }
+        }
     }
 }
 
