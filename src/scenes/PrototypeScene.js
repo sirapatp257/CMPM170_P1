4b3dcb69b88dda93d4870a5a6ee236556d8723d3
Shloak Sinha <ssinha8@ucsc.edu>
2023-10-18 04:19:38 -0700

    scene restarts abruptly - will try to smooth

--- src/scenes/PrototypeScene.js
+++ src/scenes/PrototypeScene.js
@@ -25,6 +25,7 @@ class PrototypeScene extends Phaser.Scene {
                 console.log("touched down");
                 this.balloon.sprite.setVelocity(0, 0);
                 this.balloon.sprite.setGravityY(4000);         //fine tune this for how fast the balloon falls off the map
+                this.scene.restart();
                 // this.timedEvent = this.time.delayedCall(3000, () => {
                 //     this.scene.restart();
                 // });
